90869abde06ba02be0e87f714a250c86de56f65b
Chris Hilsdon <chris@koolserve.uk>
2017-01-06 00:15:44 +0000

    More work on the orders view page

--- src/SyliusExtension.php
+++ src/SyliusExtension.php
@@ -2,12 +2,21 @@
 
 namespace Bolt\Extension\Koolserve\Sylius;
 
-use Bolt\Application;
 use Bolt\Extension\SimpleExtension;
 use Bolt\Menu\MenuEntry;
+use Silex\Application;
+use Symfony\Component\HttpFoundation\ParameterBag;
 
 class SyliusExtension extends SimpleExtension
 {
+    protected function registerServices(Application $app)
+    {
+        $app['sylius.config'] = $app->share(
+            function () {
+                return new ParameterBag($this->getConfig());
+            }
+        );
+    }
     protected function registerBackendControllers()
     {
         $config = $this->getConfig();
